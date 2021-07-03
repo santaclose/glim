9c9f20716cd601f65da45d3cca9aca306fd90bf1
san <san>
2021-07-03 23:32:39 +0200

    fix slider rendering issue

--- assets/shaders/slider.glsl
+++ assets/shaders/slider.glsl
@@ -18,6 +18,27 @@ float positiveCircle(vec2 center, float radius)
     return 1.0 - smoothstep(radius - delta / 2.0, radius + delta / 2.0, dist);
 }
 
+float udRoundBox(vec2 p, vec2 b, float r)
+{
+    return length(max(abs(p) - b + r, 0.0)) - r;
+}
+
+float positiveRoundedRectangle(vec2 topLeftCorner, vec2 bottomRightCorner)
+{
+    vec2 rectSize = bottomRightCorner - topLeftCorner;
+    vec2 rectCenter = topLeftCorner + rectSize / 2.0;
+    vec2 fixedPos = b_Pos;
+
+    rectSize.x = floor(rectSize.x);
+    rectSize.y = floor(rectSize.y);
+    rectCenter.x = floor(rectCenter.x);
+    rectCenter.y = floor(rectCenter.y);
+    rectCenter.x += 0.5 * float(mod(rectSize.x, 2.0) < 0.1);
+    rectCenter.y += 0.5 * float(mod(rectSize.y, 2.0) < 0.1);
+    vec2 p = fixedPos - rectCenter;
+    return 1.0 - clamp(udRoundBox(p, rectSize / 2.0, 3.0), 0.0, 1.0);
+}
+
 void main()
 {
     vec2 topLeftQuadPos = b_Data.rg;
@@ -33,12 +54,9 @@ void main()
     {
         float middleRailPosY = (topLeftQuadPos.y + bottomRightQuadPos.y) / 2.0;
 
-        railRectangle = float(
-            b_Pos.x > topLeftQuadPos.x + u_HandleRadius + u_SideMargin &&
-            b_Pos.x < bottomRightQuadPos.x - u_HandleRadius - u_SideMargin &&
-            b_Pos.y > middleRailPosY - u_RailThickness / 2.0 &&
-            b_Pos.y < middleRailPosY + u_RailThickness / 2.0
-            );
+        railRectangle = positiveRoundedRectangle(
+            vec2(topLeftQuadPos.x + u_HandleRadius + u_SideMargin, middleRailPosY - u_RailThickness / 2.0),
+            vec2(bottomRightQuadPos.x - u_HandleRadius - u_SideMargin, middleRailPosY + u_RailThickness / 2.0));
 
         handleCircle = positiveCircle(
             vec2(mix(topLeftQuadPos.x + u_HandleRadius + u_SideMargin, bottomRightQuadPos.x - u_HandleRadius - u_SideMargin, b_MoreData.x), middleRailPosY),
@@ -50,12 +68,10 @@ void main()
     {
         float middleRailPosX = (topLeftQuadPos.x + bottomRightQuadPos.x) / 2.0;
 
-        railRectangle = float(
-            b_Pos.y > topLeftQuadPos.y + u_HandleRadius + u_SideMargin &&
-            b_Pos.y < bottomRightQuadPos.y - u_HandleRadius - u_SideMargin &&
-            b_Pos.x > middleRailPosX - u_RailThickness / 2.0 &&
-            b_Pos.x < middleRailPosX + u_RailThickness / 2.0
-            );
+
+        railRectangle = positiveRoundedRectangle(
+            vec2(middleRailPosX - u_RailThickness / 2.0, topLeftQuadPos.y + u_HandleRadius + u_SideMargin),
+            vec2(middleRailPosX + u_RailThickness / 2.0, bottomRightQuadPos.y - u_HandleRadius - u_SideMargin));
 
         handleCircle = positiveCircle(
             vec2(middleRailPosX, mix(topLeftQuadPos.y + u_HandleRadius + u_SideMargin, bottomRightQuadPos.y - u_HandleRadius - u_SideMargin, b_MoreData.x)),
